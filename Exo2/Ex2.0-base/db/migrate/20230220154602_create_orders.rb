68b88ea4a8adbf69ac792b3cc58596540024e94c
ThomasGrossmann <47849503+ThomasGrossmann@users.noreply.github.com>
2023-02-20 16:53:24 +0100

    Create migrations

---
+++ Exo2/Ex2.0-base/db/migrate/20230220154602_create_orders.rb
@@ -0,0 +1,6 @@
+class CreateOrders < ActiveRecord::Migration[6.1]
+  def change
+    create_table :orders do |t|
+    end
+  end
+end
---
+++ Exo2/Ex2.0-base/db/migrate/20230220154612_create_orders_items.rb
@@ -0,0 +1,6 @@
+class CreateOrdersItems < ActiveRecord::Migration[6.1]
+  def change
+    create_table :orders_items do |t|
+    end
+  end
+end
