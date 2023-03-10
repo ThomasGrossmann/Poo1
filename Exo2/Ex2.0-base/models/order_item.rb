414f7718181de834b502f6789849e40331a5be80
ThomasGrossmann <47849503+ThomasGrossmann@users.noreply.github.com>
2023-03-10 10:11:59 +0100

    Exigence 5, from corrections

--- Exo2/Ex2.0-base/models/order_item.rb
+++ Exo2/Ex2.0-base/models/order_item.rb
@@ -6,9 +6,15 @@ class OrderItem < ActiveRecord::Base
   validates :order, presence: true
   validates :quantity, numericality: { greater_than: 0, only_integer: true }
 
+  before_save :set_item_price
+
   def price
     quantity * item_price
   end
 
   scope :bulk, -> (size = 100) { where("quantity >= ?", size) }
+
+  def set_item_price
+    self.item_price ||= product.price
+  end
 end
